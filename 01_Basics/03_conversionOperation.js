28998044e9b978d3d249f2b61bb2d0a6e63a2e5d
Om Jadhav <iomjadhav@gmail.com>
2025-01-05 20:00:36 +0530

    Operations on Different Data Types

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,4 +1,6 @@
-// -- Converting string to number
+// ************** Conversion **************
+// ---------- Converting string to number ------------------------
+console.log("***CONVERSION***");
 
 let marks = "95"
 console.log(marks, typeof(marks))
@@ -26,7 +28,7 @@ console.log(scoreInNumber, typeof(scoreInNumber));
 // false => 0
 // om => NaN
 
-// -- Converting number to boolean
+// -------------- Converting number to boolean ------------------------
 let isLoggedIn = 1
 console.log(isLoggedIn);
 // 1
@@ -39,7 +41,7 @@ console.log(booleanLoggedIn);
 // 1 => true
 
 
-// -- converting string to boolean 
+// --------------- converting string to boolean ----------------------
 let namee = "om"
 let booleanNamee = Boolean(namee)
 
@@ -49,3 +51,38 @@ console.log(namee, booleanNamee)
 // "" => false
 // "a" => true 
 // NOTE = If text present then true or absent
+
+// *********************** Operation ******************************
+console.log("***OPERATION***");
+
+let value =3
+let negValue = -value
+console.log(negValue)  
+
+console.log(2+2); // Additon
+console.log(2-2); // Subtraction
+console.log(2*2); // Multiplication
+console.log(2**3); // 2 to power 3
+console.log(2/3); // 2 divide by 3
+console.log(2%3); // 2 mod 3 => shows remainder
+
+// String Concatination {Addition}
+let firstName = "Om "
+let lastName = "Jadhav"
+let fullName = firstName + lastName
+console.log(fullName)
+
+console.log("1"+2)     // 12
+console.log(1+"2");    // 12
+console.log("1"+2+2);  // 122
+
+// NOTE => If any one value is String it will convert remaining value to string aswell
+
+console.log(1+2+"2"); // 32
+
+// NOTE => but if first 2 values are number and then third is string the it will consider first 2 values as numbers only 
+
+
+console.log(true) // ture
+console.log(+true); // 1 => + will convert boolean to number
+console.log(+"") // "" => is considered as false i.e 0
