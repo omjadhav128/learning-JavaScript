a949ddcaa444b9b972609031c6100cfb895afa83
Om Jadhav <iomjadhav@gmail.com>
2025-01-08 11:33:24 +0530

    For Loops

---
+++ 05_Iterator/01_For_Loop.js
@@ -0,0 +1,23 @@
+// for
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    
+}
+
+let myArray = ["thor", "ironman", "spiderman", "superman", "Batman"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+
+// break and continue
+
+// break => stop the loop when break
+// continue => only one iteration is skip, rest all loop is executed
