bd866b1b74cd292fc9bc03051c815631e5f28532
Om Jadhav <iomjadhav@gmail.com>
2025-01-08 10:17:03 +0530

    If-Else Control Statement in JS

---
+++ 04_Control_Flow/01_If_Else.js
@@ -0,0 +1,20 @@
+if(true){
+    console.log("only true block will be executed");
+}
+else{
+    console.log("this will not execute");
+}
+
+let balance = 1000 
+if(balance<500){
+    console.log("balance is less than 500");
+}
+else if (balance<800) {
+    console.log("balance is less than 800");
+}
+else if (balance<1000) {
+    console.log("balance is less than 1000");
+}
+else {
+    console.log("balance is greather than 1000");
+}
