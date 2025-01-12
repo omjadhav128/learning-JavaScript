f66471497332d1eb233544836b261370f31ee3a6
Om Jadhav <iomjadhav@gmail.com>
2025-01-12 23:41:56 +0530

    Project 1 Completed..

---
+++ 07_Project/Project 1/app.js
@@ -0,0 +1,39 @@
+let box = document.getElementsByClassName('box')
+const body = document.querySelector('body')
+
+for (let index = 0; index < box.length; index++) {
+    const element = box[index];
+    box[index].addEventListener("click",changeColor);
+}
+
+function changeColor(e) {
+    // console.log(e);
+    // console.log(e.target);
+    
+    if(e.target.id == "Red"){
+        body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id == "Green"){
+        body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id == "Yellow"){
+        body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id == "Orange"){
+        body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id == "Blue"){
+        body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id == "White"){
+        body.style.backgroundColor = e.target.id;
+        body.style.color = "black";
+    }
+    if(e.target.id == "Purple"){
+        body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id == "Grey"){
+        body.style.backgroundColor = e.target.id;
+    }
+    
+}
